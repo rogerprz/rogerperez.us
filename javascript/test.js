b71bfb86277319ca35b87958b9772b021a661141
Roger Perez <rogercodes1@gmail.com>
2017-11-06 23:27:11 -0800

    Fixed file location
    added folders and fixed filed locations.

---
+++ javascript/test.js
@@ -0,0 +1,47 @@
+
+
+var pageCount =0;
+var i=0;
+var quotePara = document.getElementById("quotePara");
+var getQuote = document.getElementById("getQuote");
+
+// Request to get json data where the quotes are located
+getQuote.addEventListener("click", function() {
+  var ourRequest= new XMLHttpRequest();
+  ourRequest.open('GET','http://rogerperez.us/quotes-1.json');
+  ourRequest.onload= function() {
+    if (ourRequest.status>= 200 && ourRequest.status<400){
+    var ourData= JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+  } else{
+    console.log("Connection Error, Please try again.")
+  }
+  };
+
+  ourRequest.send();
+
+});
+
+
+function renderHTML(data){
+  var htmlString="";
+  var htmlAuthor="";
+
+  // for (j=0;j<data.length;j++){
+  //   remove
+  // }
+  for (i=0;i<data.length; i++){
+    console.log("first",i);
+    htmlString=data[i].quote;
+    htmlAuthor=data[i].author;
+    console.log(i);
+  }
+  quotePara.textContent = data[pageCount].quote;
+  author.textContent = data[pageCount].author;
+  pageCount++;
+
+  if (pageCount>data.length-1){
+    getQuote.classList.add('hide-me');
+  }
+
+}
