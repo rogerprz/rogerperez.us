484f3fdd83f81a11bb754842157b6d343538c56f
Roger Perez <rogercodes1@gmail.com>
2017-11-12 20:12:05 -0800

    removed URL location
    removed url location

--- javascript/tweetButton.js
+++ javascript/tweetButton.js
@@ -7,7 +7,7 @@ var getAuthor=document.getElementById("author").innerHTML
 
     var tweetedLink=window.location.href;
 
-    window.open("http://twitter.com/intent/tweet?hashtags=quotes&url=" + tweetedLink +
-    "&text=" +'"'+ getPostQuote + '"' + "---"+ getAuthor + 
+    window.open("http://twitter.com/intent/tweet?hashtags=quotes&" + tweetedLink +
+    "&text=" +'"'+ getPostQuote + '"' + "---"+ getAuthor +
     "&via=iRogersb&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0");
   });
