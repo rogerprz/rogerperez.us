ff02952e4b5d30984243364e9229817d7d7bcb95
Roger Perez <rogercodes1@gmail.com>
2017-11-27 23:00:52 -0800

    Create analog clock from
    lynda.com.
    Added date.

---
+++ javascript/clockScript.js
@@ -0,0 +1,18 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+const digitalClock= document.querySelector("#digitalClock");
+let hrPosition = 20;
+let minPosition= 130;
+let secPosition= 267;
+
+var date = new Date();
+let hr=date.getHours();
+let min= date.getMinutes();
+let sec= date.getSeconds();
+
+digitalClock.document(hr+":" + min+ ":" + sec);
+HOURHAND.style.transform="rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform="rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform="rotate(" + secPosition + "deg)";
+console.log("Hello there");
