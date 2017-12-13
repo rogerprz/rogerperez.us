078e2f39833307e266b8e2f03f9c4d48825cec76
Roger Perez <rogercodes1@gmail.com>
2017-12-12 22:05:28 -0800

    testing with weather
    added geo location for testing.

--- javascript/weatherTest.js
+++ javascript/weatherTest.js
@@ -1,27 +1,38 @@
-var api= "https://fcc-weather-api.glitch.me";
+var api= "https://fcc-weather-api.glitch.me/api/current?";
+var jsonWeather= "http://rogerperez.us/JSON/weather.json";
+var ipApi= "http://ip-api.com/json";
 var x= document.getElementById("geoLocal");
 var tempX= document.getElementById("temp");
 
+
+
 var ourRequest= new XMLHttpRequest();
-ourRequest.open('GET','http://rogerperez.us/JSON/weather.json');
+ourRequest.open('GET',ipApi);
 ourRequest.onload= function() {
   if (ourRequest.status>= 200 && ourRequest.status<400){
-  var ourData= JSON.parse(ourRequest.responseText);
-  renderHTML(ourData);
+    var ourData= JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+    console.log("ourData",ourData);
 } else{
   console.log("Connection Error, Please try again.")
 }
 };
 
 ourRequest.send();
+console.log(ourRequest)
 
+// var jsonWeather= JSON.stringify(ourData);
+// document.body.innerHTML=jsonWeather;
 function renderHTML(data){
-  var temp="";
-  // var htmlAuthor="";
+  var htmlString="";
+  for (i=0;i<data.lenth;i++){
+    htmlString=data[i].coord;
+    console.log(data[i].coord)
+    tempX.textContent= data;
 
-  for (i=0;i<data.length; i++){
-    temp=data[i].weather;
-    // htmlAuthor=data[i].author;
   }
-  tempX.textContent=data[0].main.temp;
-};
+  // htmlString.textContent=data[0];
+  tempX.textContent= data;
+  // return data;
+}
+console.log(ourRequest)
