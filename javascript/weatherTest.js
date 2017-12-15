9d5cf80e6a35817b0e78b93949c54afb3fc9852c
Roger Perez <rogercodes1@gmail.com>
2017-12-14 22:26:43 -0800

    commit
    no

--- javascript/weatherTest.js
+++ javascript/weatherTest.js
@@ -1,5 +1,5 @@
 var api= "https://fcc-weather-api.glitch.me/api/current?";
-var jsonWeather= "http://rogerperez.us/JSON/weather.json";
+var apiXU= "http://api.apixu.com/v1/current.json?key=ed958f2779e542a3b5c73039171312&q=";
 var ipApi= "http://ip-api.com/json";
 var lat, lon;
 var x= document.getElementById("geoLocal");
@@ -9,10 +9,10 @@ var tempX= document.getElementById("temp");
 var geoLocal=document.getElementById("geo");
 function getLocation(){
   if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position){
-      var lon="lon="+ position.coords.longitude;
-      var lat='lat='+ position.coords.latitude;
-
+    navigator.geolocation.getCurrentPosition(
+      function getWeather(position){
+        var lat=position.coords.latitude;
+        var lon=position.coords.longitude;
       getWeather(lat,lon);
     });
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -21,50 +21,33 @@ function getLocation(){
           }
   }
 
-function showPosition(position){
-  geoLocal.innerHTML="Latitude:" + position.coords.latitude +
-  "<br> Longitude: " + position.coords.longitude;
+var finalApi=apiXU + lat+","+lon;
 
-}
 
-function getWeather(lat,lon){
-  var completeApi=api+lon+"&"+lat;
-  // lon="lon="+ position.coords.longitude;
-  // lat='lat='+ position.coords.latitude;
 
-  return completeApi;
-  }
 
 // JSON request for API to get temperature
 var ourRequest= new XMLHttpRequest();
-ourRequest.open('GET','function getWeather(lat,lon)');
+ourRequest.open('GET',finalApi);
 ourRequest.onload= function() {
   if (ourRequest.status>= 200 && ourRequest.status<400){
     var ourData= JSON.parse(ourRequest.responseText);
     renderHTML(ourData);
     console.log("ourData",ourData);
-} else{
-  console.log("Connection Error, Please try again.")
-}
-};
-
-ourRequest.send();
-console.log(ourRequest)
-
-// var jsonWeather= JSON.stringify(ourData);
-// document.body.innerHTML=jsonWeather;
-function renderHTML(data){
-  var htmlString="";
-  for (i=0;i<data.lenth;i++){
-    htmlString=data[i].coord;
-    console.log(data[i].coord)
-    tempX.textContent= data;
-
+  } else{
+    console.log("Connection Error, Please try again.")
   }
-  // htmlString.textContent=data[0];
-  tempX.textContent= data;
-  // return data;
-}
-console.log(ourRequest)
-var geoLocation= document.getElementById("geoAPI");
-geoLocation.innerHTML=completeApi;
+  };
+
+    ourRequest.send();
+    console.log(ourRequest)
+
+
+    var geoLocation= document.getElementById("geoAPI");
+    geoLocation.innerHTML=finalApi;
+
+function showPosition(position){
+    geoLocal.innerHTML="Latitude:" + position.coords.latitude +
+      "<br> Longitude: " + position.coords.longitude;
+
+    }
